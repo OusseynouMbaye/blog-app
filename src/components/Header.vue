e35c635377cd79bf0231e96e41615966e1aa079f
Ouzinem <ouzinem@hotmail.com>
2025-03-31 08:26:46 -0400

    Modification de la structure du projet : mise à jour de l'index HTML pour inclure le langage français, suppression de composants inutilisés, et simplification des styles. Remplacement de HelloWorld par un composant Header dans App.vue. Nettoyage des fichiers CSS et suppression de fichiers de composants non nécessaires.

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><strong>Blog App</strong></li>
+      </ul>
+      <ul>
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/about">About</RouterLink></li>
+      </ul>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
